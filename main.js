b42cb1a7d0b0c0d866e09b1c191008629c711388
Tim Mok <timothyjmok@gmail.com>
2021-04-12 09:44:04 -0400

    updated dashboard, top headlines functionality

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 console.log('Reporting from main.js')
 
 // DOM Selectors
+const navWelcome = document.querySelector('#nav-welcome')
 const navAbout = document.querySelector('#nav-about')
 const navSignIn = document.querySelector('#nav-signin')
 const navSignUp = document.querySelector('#nav-signup')
@@ -26,10 +27,23 @@ const emailSignUp = document.querySelector('#signup-email')
 const passwordSignUp = document.querySelector('#signup-password')
 const countrySignUp = document.querySelector('#signup-country')
 
+const dashUserName = document.querySelector('#dash-username')
+const dashCountry = document.querySelector('#dash-country')
 
+const dashSearchBar = document.querySelector('#search-bar')
+const dashSearchButton = document.querySelector('#search-button')
+
+const dashHeadlinesArea = document.querySelector('#headlines-area')
+const dashSearchArea = document.querySelector('#search-area')
+const dashBookmarksArea = document.querySelector('#bookmarks-area')
+
+const testButton = document.getElementById('test-headlines')
+
+let headlines = []
 
 // Reusable Functions
 const navLoggedIn = () => {
+    navAbout.classList.add('hidden')
     navSignIn.classList.add('hidden')
     navSignUp.classList.add('hidden')
     navDashboard.classList.remove('hidden')
@@ -38,6 +52,7 @@ const navLoggedIn = () => {
 }
 
 const navLoggedOut = () => {
+    navAbout.classList.remove('hidden')
     navSignIn.classList.remove('hidden')
     navSignUp.classList.remove('hidden')
     navDashboard.classList.add('hidden')
@@ -56,7 +71,19 @@ const clearDOM = (area) => {
     }  
 }
 
+const showDashUserName = (userName) => {
+    dashUserName.innerText = userName
+}
+
+const showDashCountry = (country) => {
+    dashCountry.innerText = country
+}
+
 // Nav-link Functions
+navWelcome.addEventListener('click', () => {
+    showSection('.welcome')
+})
+
 navAbout.addEventListener('click', () => {
     showSection('.about')
 })
@@ -69,9 +96,18 @@ navSignUp.addEventListener('click', () => {
     showSection('.signup')
 })
 
+navDashboard.addEventListener('click', () => {
+    showSection('.dashboard')
+})
+
+navSettings.addEventListener('click', () => {
+    showSection('.settings')
+})
+
 navLogout.addEventListener('click', () => {
     navLoggedOut()
     showSection('.logout')
+    clearDOM(dashHeadlinesArea)
     localStorage.clear()
 })
 
@@ -86,19 +122,21 @@ formSignUp.addEventListener('submit', async (e) => {
     const country = countrySignUp.value
 
     try {
-        const response = await axios.post('http://localhost:3001/users/', {
+        const response = await axios.post('http://localhost:3001/users/signup', {
             name: name,
             email: email,
             password: password,
             country: country
         })
 
-
-
         const userId = response.data.user.id
+        const userName = response.data.user.name
+        const countryName = response.data.country.name
         localStorage.setItem('userId', userId)
             navLoggedIn()
             showSection('.dashboard')
+            showDashUserName(userName)
+            showDashCountry(countryName)
             
     } catch (error) {
         alert('Email already taken')
@@ -109,7 +147,7 @@ formSignIn.addEventListener('submit', async (e) => {
     e.preventDefault()
 
     const email = emailSignIn.value
-    const password = emailSignIn.value
+    const password = passwordSignIn.value
 
     try {
         const response = await axios.post('http://localhost:3001/users/signin', {
@@ -117,12 +155,123 @@ formSignIn.addEventListener('submit', async (e) => {
             password: password
         })
 
-        const userId = response.userId
+        const userId = response.data.user.id
+        const userName = response.data.user.name
+        const countryName = response.data.country.name
         localStorage.setItem('userId', userId)
+            navLoggedIn()
+            showSection('.dashboard')
+            showDashUserName(userName)
+            showDashCountry(countryName)
         
             
     } catch (error) {
         alert('Login failed')
     }
 })
+
+
+
+
+// Dashboard functionality (top headlines, search bar & results, user bookmarks)
+const topHeadlines = async () => {
+    try {
+        const userId = localStorage.getItem('userId')
+        
+        const response = await axios.get('http://localhost:3001/news/headlines/', {
+            headers: {
+                authorization: userId
+            }
+        })
+        console.log(response.data)
+        
+    } catch (error) {
+        alert('Getting top headlines failed')
+    }
+}
+
+const showHeadlines = (response) => {
+    clearDOM(dashHeadlinesArea)
+    headlines = response.data.articles
+
+    for ( let i = 0; i < headlines.length; i++ ) {
+        let headlineComponent = document.createElement('a')
+        headlineComponent.classList.add('headline-component')
+        headlineComponent.href = `${headlines[i].url}`
+        headlineComponent.target = '_blank'
+        dashHeadlinesArea.appendChild(headlineComponent)
+
+        let headlineTitle = document.createElement('div')
+        headlineTitle.classList.add('headline-title')
+        headlineTitle.innerText = `${headlines[i].title}`
+        headlineComponent.appendChild(headlineTitle)
+
+        let headlineImage = document.createElement('img')
+        if ( headlines[i].urlToImage !== null ) {
+            headlineImage.src = `${headlines[i].urlToImage}`
+        } else {
+            headlineImage.src = 'https://images.unsplash.com/photo-1504711434969-e33886168f5c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+        }
+        headlineImage.classList.add('headline-image')
+        headlineComponent.appendChild(headlineImage)
+
+        let headlineSave = document.createElement('button')
+        headlineSave.classList.add('headline-save')
+        headlineSave.innerText = 'Bookmark'
+        headlineComponent.appendChild(headlineSave)
+    }
+
+}
+
+
+testButton.addEventListener('click', async () => {
+    try {
+        const userId = localStorage.getItem('userId')
+        
+        const response = await axios.get('http://localhost:3001/news/headlines/', {
+            headers: {
+                authorization: userId
+            }
+        })
+        console.log(response)
+        showHeadlines(response)
+    } catch (error) {
+        alert('Getting top headlines failed')
+    }
+})
+
+
+// On load
+const pageOnLoad = async () => {
+    try {
+        const userId = localStorage.getItem('userId')
+
+        if (userId) {
+            const response = await axios.get('http://localhost:3001/users', {
+                headers: {
+                    authorization: userId
+                }
+            })
+            const userName = response.data.user.name
+            const countryName = response.data.country.name
+                navLoggedIn()
+                showSection('.dashboard')
+                showDashUserName(userName)
+                showDashCountry(countryName)
+                
+
+        } else {
+            navLoggedOut()
+        }
+
+
+    } catch (error) {
+        alert('Page load failed')
+    }
+}
+
+
+
+
+document.addEventListener('DOMContentLoaded', pageOnLoad())
 
