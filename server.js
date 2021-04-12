c265bd417216c80ae2b91489ad04ba1c6147bee9
Tim Mok <timothyjmok@gmail.com>
2021-04-12 16:42:52 -0400

    prepared for frontend heroku deployment

--- server.js
+++ server.js
@@ -4,14 +4,24 @@ const app = express()
 const routesReport = require('rowdy-logger').begin(app)
 
 const path = require('path')
+const replaceInFile = require('replace-in-file')
 
 app.get('/', (req, res) => {
   const filepath = path.join(__dirname, 'index.html')
   res.sendFile(filepath)
 })
 
-app.get('/main.js', (req, res) => {
+app.get('/main.js', async (req, res) => {
   const filepath = path.join(__dirname, 'main.js')
+
+  if (process.env.NODE_ENV === 'production') {
+    await replaceInFile({
+      files: filepath,
+      from: 'http://localhost:3001',
+      to: 'https://backend-sei-solo-project-2.herokuapp.com/'
+    })
+  }
+
   res.sendFile(filepath)
 })
 
@@ -20,7 +30,7 @@ app.get('/style.css', (req, res) => {
   res.type('css').sendFile(filepath)
 })
 
-const port = process.env.PORT || 3000
-app.listen(port, () => {
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
     routesReport.print()
 })
